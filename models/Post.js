749ef739b61620f47a790f9c913a91733a0a4f07
Kapil <87026179+kandelkapil@users.noreply.github.com>
2021-10-09 01:58:22 +0545

    minor changes

--- models/Post.js
+++ models/Post.js
@@ -25,35 +25,4 @@ const PostSchema = mongoose.Schema({
 
 module.exports = mongoose.model("Post", PostSchema);
 
-/* 
-user ko stock hunxa
 
-share detail = buying selling transaction.
-holding table share fk, buying , sp, user
-
-user name email
-
-
-manip nabil, laxmi
-kapil bok, lumbini
-
-
-[kapil manip]
-
-
-dashboard api
-
-logged in user patta laune(kapil)
-
-stock table ma user query as per the logged in user
-holding table query fetch record where user is kapil
-
-
-queryset = [bok, lumbini]
-
-for loop in query set:
-
-share instance 
-sbok of user kapil fetch
-
-*/
