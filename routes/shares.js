9dec43856bc9feac392c390408860dc8488d32e5
Kapil <87026179+kandelkapil@users.noreply.github.com>
2021-10-26 11:01:15 +0545

    Added  numerous fields

--- routes/shares.js
+++ routes/shares.js
@@ -4,20 +4,23 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const post = await Post.find();
-    res.json(post);
+    const posts = await Post.find();
+    res.json(posts);
   } catch (error) {
     res.json({ message: error });
   }
 });
 
 router.post("/", async (req, res) => {
+  const body = req.body;
   const post = new Post({
-    name: req.body.name,
-    number_of_stocks: req.body.number_of_stocks,
-    status: req.body.status,
-    price_per_unit: req.body.price_per_unit,
-    transaction_date: req.body.transaction_date,
+    name: body.name,
+    price_of_stock: body.price_of_stock,
+    status: body.status,
+    no_of_units: body.no_of_units,
+    transaction_date: body.transaction_date,
+    user_value: body.price_of_stock * body.no_of_units,
+    market_value: body.market_value,
   });
 
   try {
